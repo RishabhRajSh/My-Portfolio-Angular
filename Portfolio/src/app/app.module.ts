b4fe9a75518c1d378bf4d42cae6161e0e02ce48b
RishabhRajSh <70883764+RishabhRajSh@users.noreply.github.com>
2024-03-03 01:23:18 +0530

    Expertise Section Added

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HerosectionComponent } from './components/herosection/herosection.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HerosectionComponent
+    HerosectionComponent,
+    ExpertiseComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +26,10 @@ import { HerosectionComponent } from './components/herosection/herosection.compo
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatRippleModule,
+    MatGridListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
