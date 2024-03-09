8a262d7b480437c48644c6c64e9a0045e85cc2aa
RishabhRajSh <70883764+RishabhRajSh@users.noreply.github.com>
2024-03-09 22:47:36 +0530

    image animation achieved

--- Portfolio/src/app/components/atoms/image-box/image-box.component.ts
+++ Portfolio/src/app/components/atoms/image-box/image-box.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
 @Component({
   selector: 'app-image-box',
   templateUrl: './image-box.component.html',
   styleUrls: ['./image-box.component.scss']
 })
-export class ImageBoxComponent {
+export class ImageBoxComponent implements AfterViewInit {
+  @ViewChild('container') container!: ElementRef;
 
- 
+  constructor() {}
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => this.handleIntersection(entries));
+    observer.observe(this.container.nativeElement);
+  }
+
+  handleIntersection(entries: IntersectionObserverEntry[]) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }
 }
